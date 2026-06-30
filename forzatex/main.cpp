385f5c0bd88d8b036db64fc08c51530641b52614
Apex <apex@quixel.net>
2026-06-30 09:40:26 -0300

    refactored

--- forzatex/main.cpp
+++ forzatex/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <ctime>
 #include <sstream>
+#include <array>
 
 #include "BundleReader.hpp"
 #include "TextureResolver.hpp"
@@ -11,11 +12,11 @@
 #include "xbg.h"
 #include "core.h"
 
-int GetVersionByMagic(const std::string& file)
+int GetVersionByMagic(const std::string& file, std::unique_ptr<std::byte[]>& buffer, int* size)
 {
-	char bundle_magic[4] = { 0x62, 0x75, 0x72, 0x47 };
-	char xds_offset_v1[4] = { 0x00, 0x00, 0x00, 0x03 };
-	char xds_offset_v2[4] = { 0x00, 0x00, 0x00, 0x01 };
+	std::array<uint8_t, 4> bundle_magic{ 0x62, 0x75, 0x72, 0x47 };
+	std::array<uint8_t, 4> xds_offset_v1{ 0x00, 0x00, 0x00, 0x03 };
+	std::array<uint8_t, 4> xds_offset_v2{ 0x00, 0x00, 0x00, 0x01 };
 
 	if (!std::filesystem::exists(file))
 	{
@@ -29,37 +30,52 @@ int GetVersionByMagic(const std::string& file)
 		return -1;
 	}
 
-	char stream_magic_v1[4];
-	stream.read(reinterpret_cast<char*>(&stream_magic_v1), 4);
+	std::array<uint8_t, 4> stream_magic_v1{};
+	stream.read(reinterpret_cast<char*>(stream_magic_v1.data()), 4);
 
-	char stream_magic_v2[4];
-	stream.read(reinterpret_cast<char*>(&stream_magic_v2), 4);
+	std::array<uint8_t, 4> stream_magic_v2{};
+	stream.read(reinterpret_cast<char*>(stream_magic_v2.data()), 4);
 
-	if (std::memcmp(bundle_magic, stream_magic_v1, 4) == 0)
+	stream.seekg(0, std::ios::end);
+
+	if (std::memcmp(bundle_magic.data(), stream_magic_v1.data(), 4) == 0)
 	{
+		*size = static_cast<int>(stream.tellg());
+		buffer = std::make_unique<std::byte[]>(*size);
+		stream.seekg(0, std::ios::beg);
+
+		stream.read(reinterpret_cast<char*>(buffer.get()), *size);
+		stream.close();
+
 		return 1;
 	};
 
-	if (std::memcmp(xds_offset_v1, stream_magic_v1, 4) == 0 && std::memcmp(xds_offset_v2, stream_magic_v2, 4) == 0)
+	if (std::memcmp(xds_offset_v1.data(), stream_magic_v1.data(), 4) == 0 && std::memcmp(xds_offset_v2.data(), stream_magic_v2.data(), 4) == 0)
 	{
+		*size = static_cast<int>(stream.tellg());
+		buffer = std::make_unique<std::byte[]>((*size) + 0x30); // reserve memory to xds header
+		stream.seekg(0, std::ios::beg);
+
+		std::fill(buffer.get(), buffer.get() + 0x30, std::byte{0});
+
+		stream.read(reinterpret_cast<char*>(buffer.get()) + 0x30, *size);
+		stream.close();
+
 		return 2;
 	};
 
 	return 0;
 }
 
-
 int main(int argc, char* args[])
 {
-	const char* VERSION = "1.3.0";
+	const char* VERSION = "1.3.1";
 
 	if (argc > 1)
 	{
 		for (size_t i = 1; i < argc; ++i)
 		{
 			std::string file_input(args[i]);
-			std::filesystem::path file_extension = std::filesystem::path(file_input).extension();
-
 
 			if (!std::filesystem::exists(file_input))
 			{
@@ -68,20 +84,23 @@ int main(int argc, char* args[])
 				return 1;
 			}
 
-			switch (GetVersionByMagic(file_input))
+			std::unique_ptr<std::byte[]> buffer = nullptr;
+			int size = 0;
+
+			switch (GetVersionByMagic(file_input, buffer, &size))
 			{
 			case 0:
 			{
-				std::cout << "Unsupported file extension: " << file_extension << "\n";
+				std::cout << "Unsupported file extension: " << std::filesystem::path(file_input).extension() << "\n";
 				break;
 			}
 			case 1: // file_extension == ".swatchbin"
 			{
-				std::cout << "ForzaTech Graphics Converter v" << VERSION << "-release+" << GetCoreVersion() << "\n";
+				std::cout << "ForzaTech Graphics Converter v" << VERSION << "-release+" << GetCoreBuild() << "\n";
 
 				std::filesystem::path file_output = std::filesystem::path(file_input).replace_extension(".dds");
 
-				auto reader = std::make_unique<fmnext::BundleReader>(file_input);
+				auto reader = std::make_unique<fmnext::BundleReader>(buffer.get(), size);
 				if (reader->Init())
 				{
 					if (!reader->bundle.Textures.empty())
@@ -103,12 +122,13 @@ int main(int argc, char* args[])
 			}
 			case 2: //file_extension == ".xds"
 			{
-				std::cout << "Forza Graphics Converter v" << VERSION << "-release+" << GetCoreVersion() << "\n";
+				std::cout << "Forza Graphics Converter v" << VERSION << "-release+" << GetCoreBuild() << "\n";
 
-				HRESULT res = ReadXDSFile(file_input.c_str());
-				if (res != S_OK)
+				std::filesystem::path file_output = std::filesystem::path(file_input).replace_extension(".tga");
+
+				if (CreateXDSBuffer(buffer.get(), &size) == S_OK)
 				{
-					//return 0;
+					Convert(buffer.get(), size, file_output.string().c_str());
 				}
 
 				break;
@@ -121,21 +141,10 @@ int main(int argc, char* args[])
 			}
 		}
 	}
-	else 
+	else
 	{
 		std::cout << "Usage: " << args[0] << " filename.swatchbin/xds" << "\n";
-	} 
+	}
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
